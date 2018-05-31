b3fb1052a45dc24d0e8f1036e84ba2b92d500cbd
Aquiles Valdivia <mail@aquiles.de>
2018-05-31 13:14:11 +0200

    search function

--- app/controllers/kindergardens_controller.rb
+++ app/controllers/kindergardens_controller.rb
@@ -1,19 +1,31 @@
 class KindergardensController < ApplicationController
 
   def index
-    @kindergardens = Kindergarden.where.not(latitude: nil, longitude: nil)
+    if params[:query].present?
+      @kindergardens = Kindergarden.near(params[:query], 3, :units => :km)
 
-    @markers = @kindergardens.map do |kindergarden|
-      {
-        lat: kindergarden.latitude,
-        lng: kindergarden.longitude,
-        infoWindow: { content: render_to_string(partial: "/kindergardens/map_box", locals: { kindergarden: kindergarden }) }
-      }
+      @markers = @kindergardens.map do |kindergarden|
+        {
+          lat: kindergarden.latitude,
+          lng: kindergarden.longitude,
+          infoWindow: { content: render_to_string(partial: "/kindergardens/map_box", locals: { kindergarden: kindergarden }) }
+        }
+      end
+    else
+      @kindergardens = Kindergarden.where.not(latitude: nil, longitude: nil)
+      @markers = @kindergardens.map do |kindergarden|
+        {
+          lat: kindergarden.latitude,
+          lng: kindergarden.longitude,
+          infoWindow: { content: render_to_string(partial: "/kindergardens/map_box", locals: { kindergarden: kindergarden }) }
+        }
+      end
     end
   end
 
   def show
     @kindergarden = Kindergarden.find(params[:id])
   end
+
 
 end
--- app/models/kindergarden.rb
+++ app/models/kindergarden.rb
@@ -12,6 +12,6 @@ class Kindergarden < ApplicationRecord
   has_many :properties, :through => :kita_properties
 
   geocoded_by :address
-  # after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_address?
 
 end
