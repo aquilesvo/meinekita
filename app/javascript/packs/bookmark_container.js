4834ad1d80ead5e2528dcb74779ef10c64e2721f
Raffael Kniep <leus_raff@yahoo.de>
2018-06-05 14:34:55 +0200

    bookmarks work once then you need to refresh

--- app/javascript/packs/bookmark_container.js
+++ app/javascript/packs/bookmark_container.js
@@ -1,11 +1,3 @@
-document.querySelectorAll(".bookmark-symbol").forEach((bookmark) => {
-  bookmark.addEventListener("click", (event) => {
-
-  event.currentTarget.classList.toggle("selected-bookmark");
-
-});
-});
-
 document.querySelectorAll(".bookmark-container").forEach((bookmark) => {
   bookmark.addEventListener("click", (event) => {
 
@@ -13,5 +5,3 @@ document.querySelectorAll(".bookmark-container").forEach((bookmark) => {
 
 });
 });
-
-
