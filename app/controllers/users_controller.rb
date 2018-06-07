724b9096c6de24af6aea956cfbb921f9268293a6
Raffael Kniep <leus_raff@yahoo.de>
2018-06-07 17:58:45 +0200

    some changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:show, :update]
+before_action :set_user, only: [:show, :update, :remove_photo]
 
   def show
     @inquiries_own = Inquiry.where(user_id: current_user)
@@ -9,17 +9,24 @@ before_action :set_user, only: [:show, :update]
 
   def update
     @user = current_user
-    user.update(current_user_params)
+    current_user.update(current_user_params)
     redirect_to user_path(@user)
   end
 
   def destroy
   end
 
+  def remove_photo
+    @user = current_user
+    @user.remove_photo!
+    @user.save
+    redirect_to user_path(@user)
+  end
+
   private
 
   def current_user_params
-    params.require(:user).permit(:email)
+    params.require(:user).permit(:email, :photo)
   end
 
   def set_user
--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,4 +1,10 @@
 class Inquiry < ApplicationRecord
   belongs_to :kindergarden
   belongs_to :user
+
+  def alert
+    super ? Time.at(super) : super
+  end
 end
+
+
